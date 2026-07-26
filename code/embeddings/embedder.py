217c9890d3bd6890daaf6f273176fc8f1ee100e8
Yash Pandey <ypandey474@gmail.com>
2026-07-26 13:46:18 +0530

    Add requirements

--- code/embeddings/embedder.py
+++ code/embeddings/embedder.py
@@ -7,18 +7,32 @@ logger = logging.getLogger(__name__)
 class Embedder:
     model: FlagAutoModel
     _embedding_dimension: int
+    device: str
 
-    def __init__(self, model_name: str = 'BAAI/bge-large-en-v1.5'):
+    def __init__(
+            self,
+            model_name: str = 'BAAI/bge-large-en-v1.5',
+            device: str = 'cpu'
+        ):
         try:
-            self.model: FlagAutoModel = FlagAutoModel.from_finetuned(model_name, use_fp16=True)
+
+            if not device or not device.lower() in ['cuda', 'cpu']:
+                raise ValueError(f"Device value is invalid: {device}")
+
+            self.device = device
+            
+            self.model: FlagAutoModel = FlagAutoModel.from_finetuned(model_name, use_fp16=device == 'cuda')
             logger.info(f"Loaded model: {model_name} for generating embeddings")
             self._embedding_dimension = self.model.model.config.hidden_size
             logger.info(f"For model: {model_name}, embedding dimension: {self.embedding_dimension}")
+
+
         except Exception as e:
             logger.exception(f"Could not load embedding model due to error: {e}, cannot embed documents.")
             raise # construct valid objects or don't construct them at all
 
     def embed(self, text: str) -> np.ndarray:
+        logger.info(f"Generating embedding for text of length: {len(text)}")
         return self.embed_batch([text])[0]
     
     def embed_batch(self, texts: list[str], batch_size: int = 32) -> np.ndarray:
@@ -32,8 +46,10 @@ class Embedder:
             A NumPy array of shape (len(texts), embedding_dimension).
         """
         if not texts:
+            logger.error(f"Empty array of texts provided to embedder, nothing to embed")
             return np.empty((0, 0), dtype=np.float32)
-        
+
+        logger.info(f"Generating embedding for {len(texts)} texts using batch size of {batch_size}")
         return self.model.encode(texts, normalize_embeddings=True, batch_size=batch_size)
     
     @property
